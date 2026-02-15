908caf0916d4ca08ae4583185e833404ed8893c3
Kia <94079553+notkearash@users.noreply.github.com>
2026-02-15 18:00:24 -0500

    fix: disable tiptap immediate render

--- client/components/document/TipTapEditor.tsx
+++ client/components/document/TipTapEditor.tsx
@@ -22,6 +22,7 @@ export function TipTapEditor({ initialContent, onUpdate }: TipTapEditorProps) {
       }),
     ],
     content: initialContent,
+    immediatelyRender: false,
     onUpdate: ({ editor: ed }) => {
       onUpdateRef.current(ed);
     },
